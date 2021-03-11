3d9cbce62377e0284fc92aafef03cc705ef38ef3
Adam Bieńkowski <donadigos159@gmail.com>
2021-03-11 21:10:51 +0100

    Add new sets of blocks

--- pygbx/__init__.py
+++ pygbx/__init__.py
@@ -1,5 +1,8 @@
 from pygbx.headers import CGameHeader, CGameCtnCollectorList, CollectorStock, MapBlock, Vector3, CGameChallenge, CGameBlockItem, CGameWaypointSpecialProperty, CGameCommon, CGameReplayRecord, CGameGhost, CGameCtnGhost, ControlEntry, GhostSampleRecord
 from pygbx.bytereader import ByteReader
 from pygbx.stadium_blocks import STADIUM_BLOCKS
+from pygbx.canyon_blocks import CANYON_BLOCKS
+from pygbx.valley_blocks import VALLEY_BLOCKS
+from pygbx.lagoon_blocks import LAGOON_BLOCKS
 from pygbx.stadium_block_offsets import STADIUM_BLOCK_OFFSETS
 from pygbx.gbx import Gbx, GbxType, GbxLoadError
